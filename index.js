c85ee12c0d8907dc241f1caafd1af55777924ce3
Arvin Yardhika <arvinyardhika@gmail.com>
2020-12-02 12:10:19 +0700

    Hello World

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const port = 3000
 const app = express()
 
 app.get('/',(req,res)=>{
-    console.log('API Selamat Pagi')
-    res.send('Selamat Pagi');
+    console.log('Hello world')
+    res.send('Hello World');
 })
 
 app.listen(port,() => console.log('API listening'))
