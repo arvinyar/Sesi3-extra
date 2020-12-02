8e2586bfc50e015d1365e4d765ec4903b473207e
Arvin Yardhika <arvinyardhika@gmail.com>
2020-12-02 11:48:29 +0700

    Change response to selamat pagi

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const port = 3000
 const app = express()
 
 app.get('/',(req,res)=>{
-    console.log('API Hello World')
-    res.send('Hello World');
+    console.log('API Selamat Pagi')
+    res.send('Selamat Pagi');
 })
 
 app.listen(port,() => console.log('API listening'))
